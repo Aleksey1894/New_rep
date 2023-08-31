d1f8636bbcc7d4eb6f515688d1ebc33d4bbf26d8
Aleksey1894 <oleksii.litvinchuk94@gmail.com>
2023-08-31 23:18:38 +0300

    Branch_2_commit

--- dz01/Program.cs
+++ dz01/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Oleksii!");
 Console.WriteLine("Real Madrid");
+Console.WriteLine("Branch 2");
 Console.ReadLine();
